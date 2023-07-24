85e004ac2703120c6e69cb63f9c6690c66023d6f
NikolayH <webcaster@mail.ru>
2023-07-24 20:26:48 +0300

    Added

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,10 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(i) {
+
+  return i;
+}
 
 /*
  Задание 2:
@@ -28,7 +31,13 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a, b = 100) {
+
+  return a + b;
+
+}
+
+
 
 /*
  Задание 3:
@@ -38,7 +47,11 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(func) {
+
+  return func();
+}
+
 
 /*
  Задание 4:
@@ -53,7 +66,20 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(number = 0) {
+
+  let n = number;
+
+  function inc() {
+
+    n++;
+    return n;
+
+  }
+
+  return inc;
+}
+
 
 /*
  Задание 5 *:
@@ -64,7 +90,11 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...numbers) {
+
+  return numbers
+
+ }
 
 export {
   returnFirstArgument,
