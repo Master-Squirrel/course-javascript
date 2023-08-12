267dfd332a7bd3203b521db3fec809ea62570488
NikolayH <webcaster@mail.ru>
2023-08-12 18:26:21 +0300

    Added

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,8 +1,6 @@
 import photosDB from './photos.json';
 import friendsDB from './friends.json';
-
 import pages from './pages';
-
 import('./styles.css');
 
 const pageNames = ['login', 'main', 'profile'];
@@ -10,41 +8,39 @@ const pageNames = ['login', 'main', 'profile'];
 document.addEventListener('click', () => {
 
     const pageToOpen = getRandomElement(pageNames);
-    console.log(pages)
+    
     pages.openPage(pageToOpen)
-   
 
 });
 
 function getRandomElement(array) {
-        
+
     if (!array.length) {
         return null;
     }
-    
-    const index = Math.round((Math.random() * (array.length -1)));
+
+    const index = Math.round((Math.random() * (array.length - 1)));
 
     return array[index];
 }
 
-
 export default {
 
-    
-    getNextPhoto () {
 
-        let randomFrend = this.getRandomElement(friendsDB);        
+    getNextPhoto() {
+
+        let randomFrend = this.getRandomElement(friendsDB);
         let photos = photosDB[randomFrend.id]
         let randomPhoto = this.getRandomElement(photos)
-        
-    
+
+
         return requiredFriend = {
-            randomFrend, url : photos.url            
+            randomFrend, url: photos.url
         }
-        
+
     }
 
-    
+
 }
 
 
--- projects/loft-photo/pages.js
+++ projects/loft-photo/pages.js
@@ -2,29 +2,20 @@ const pagesMap = {
     login: '.page-login',
     main: '.page-main',
     profile: '.page-profile',
-
-    openPage(name) {
-
-        this.hideAllPages();
-
-        console.log(name);
-
-        const div = document.querySelector(`.page-${name}`);        
-        div.classList.remove('hidden');
-        
-    },
-
-    hideAllPages() {
-        document.querySelector('.page-login').classList.add('hidden');
-        document.querySelector('.page-main').classList.add('hidden');
-        document.querySelector('.page-profile').classList.add('hidden');
-    }
   };
   
-  
+  let currentPage = null;
 
   export default {
     openPage(name) {
+
+      const selector =  pagesMap[name];
+      const element = document.querySelector(selector);
+         
+      currentPage?.classList.add('hidden');
+      currentPage = element;
+      currentPage.classList.remove('hidden');
+    
     },
   };
 
